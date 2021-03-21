2a38c67ebee11e5556f8c0bef94e662b8937fe56
Bruno <bruno.fdm93@gmail.com>
2021-03-20 23:52:26 -0700

    login working

--- src/components/SignIn/SignIn.tsx
+++ src/components/SignIn/SignIn.tsx
@@ -18,26 +18,12 @@ import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
 
-const SignIn: React.FC = (props) => {
-  const allUsers = useAppSelector((state) => state.users);
-  const [emailLogin, setEmailLogin] = useState<string>("");
-  const [passLogin, setPassLogin] = useState<string>("");
+const SignIn: React.FC = () => {
+  const allUsers = useAppSelector((state) => {
+    return state.users;
+  });
+
   const classes = useStyles();
-
-  useEffect(() => {
-    emailAuthenticated = null;
-  }, [emailLogin]);
-
-  // const formik = useFormik({
-  //   initialValues: {
-  //     email: "aa",
-  //     password: "aa",
-  //   },
-  //   validationSchema: validationSchema,
-  //   onSubmit: (values: React.FormEvent<HTMLFormElement>) => {
-  //     alert(JSON.stringify(values, null, 2));
-  //   },
-  // });
 
   const validationSchema = yup.object({
     email: yup
@@ -57,27 +43,22 @@ const SignIn: React.FC = (props) => {
     },
     validationSchema: validationSchema,
     onSubmit: (values) => {
-      console.log("values ", values);
-
-      const validFields = validate(values);
-      // if (validFields) {
-      //   formik.setErrors({ email: "errotyp" });
-      // }
-
-      const authenticatedUser = authenticate(values);
+      authenticate(values);
     },
   });
 
-  const FormRefs = {
-    email: useRef<HTMLDivElement | null>(null),
-    password: useRef<HTMLDivElement | null>(null),
+  const emailExistsInDb = (email: string) => {
+    return new Promise((resolve, reject) => {
+      allUsers.find((el) => {
+        const emailExists = el.authentication.email === email;
+        if (emailExists) {
+          return resolve(el.authentication.password);
+        } else {
+          return reject("Your email is not in our system.");
+        }
+      });
+    });
   };
-  const validate = ({ email }: any): boolean => {
-    return validator.isEmail(email);
-    //message: "Please provide a valid Email",
-  };
-
-  let emailAuthenticated: typeof allUsers[0] | undefined | null;
 
   const authenticate = ({
     email,
@@ -86,45 +67,22 @@ const SignIn: React.FC = (props) => {
     email: string;
     password: string;
   }) => {
-    if (!emailAuthenticated) {
-      emailAuthenticated = allUsers.find(
-        (el) => el.authentication.email === email
-      );
-      if (!emailAuthenticated) {
-        return formik.setErrors({ email: "Your email is not in our system." });
-      }
-    }
-    if (emailAuthenticated.authentication.password === passLogin) {
-      return { result: true };
-    } else {
-      return {
-        field: SI.FormField.PASSWORD,
-        result: false,
-        message: "Wrong Password",
-      };
-    }
+    const emailExists = emailExistsInDb(email);
+
+    emailExists
+      .then((response: any) => {
+        if (response !== password) {
+          return formik.setErrors({
+            password: "Wrong Password!",
+          });
+        }
+      })
+      .catch((response) => {
+        return formik.setErrors({
+          email: response,
+        });
+      });
   };
-
-  const handleFormError = (error: SI.AuthErrorObject) => {
-    console.log(FormRefs[error.field].current);
-  };
-
-  // const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-  //   e.preventDefault();
-  //
-  // };
-
-  const handleChange = (
-    e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
-  ) => {
-    if (e.target.name === SI.FormField.EMAIL) {
-      setEmailLogin(e.target.value);
-    } else {
-      setPassLogin(e.target.value);
-    }
-  };
-
-  console.log("PROPZ", props);
 
   return (
     <Container component="main" maxWidth="xs">
@@ -136,7 +94,11 @@ const SignIn: React.FC = (props) => {
         <Typography component="h1" variant="h5">
           Sign in
         </Typography>
-
+        <Typography component="h1" variant="subtitle2">
+          <span>Email: user@example.com</span>
+          <br />
+          <span>Password: helloworld</span>
+        </Typography>
         <form className={classes.form} onSubmit={formik.handleSubmit}>
           <TextField
             variant="outlined"
@@ -188,67 +150,3 @@ const SignIn: React.FC = (props) => {
 };
 
 export default SignIn;
-
-{
-  /* <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign in
-        </Typography>
-        <form className={classes.form} onSubmit={(e) => handleSubmit(e)}>
-          <TextField
-            ref={FormRefs[SI.FormField.EMAIL]}
-            value={emailLogin}
-            onChange={(e) => handleChange(e)}
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name={SI.FormField.EMAIL}
-            autoComplete="email"
-            autoFocus
-          />
-          <TextField
-            ref={FormRefs[SI.FormField.PASSWORD]}
-            value={passLogin}
-            onChange={(e) => handleChange(e)}
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name={SI.FormField.PASSWORD}
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-          />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Sign In
-          </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2">
-                Forgot password?
-              </Link>
-            </Grid>
-          </Grid>
-        </form>
-      </div>
-    </Container> */
-}
